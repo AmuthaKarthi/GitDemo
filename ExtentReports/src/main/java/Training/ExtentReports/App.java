ffeedf7fc90e808c0fb82ccec7785909adb96790
AmuthaKarthi <amutha.ame.k@gmail.com>
2024-12-08 23:55:43 +0800

    this is new commit

--- ExtentReports/src/main/java/Training/ExtentReports/App.java
+++ ExtentReports/src/main/java/Training/ExtentReports/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World1" );
+        System.out.println( "Hello World2" );
+        System.out.println( "Hello World3" );
+        System.out.println( "Hello World4" );
+        System.out.println( "Hello World5" );
+        System.out.println( "Hello World6" );
+        
     }
 }
